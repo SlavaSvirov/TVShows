7d485be2c3bf39e9fd586ea2e400453cdfe5e54e
Вячеслав Свиров <slavasvirov@MacBook-Pro-Vaceslav.local>
2021-03-10 14:43:03 +0300

    change search bar

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Input, Checkbox } from "antd";
+import { Input, Space, Checkbox } from "antd";
 import styles from "./Search.module.css";
 
 function Search({ onChange, filterByImage, placeholder, text }) {
   return (
     <div className={styles.inputBar}>
-      <Input
+      <Input autoFocus type='search' className={styles.inputRow} placeholder={placeholder} onChange={onChange} />
+      {/* <Input
         type="search"
         className={styles.inputRow}
         onChange={onChange}
         placeholder={placeholder}
         size="800px"
         autoFocus
-      />
+      /> */}
       <Checkbox className={styles.checkboxCustom} onChange={filterByImage}>
         С изображениями
       </Checkbox>
