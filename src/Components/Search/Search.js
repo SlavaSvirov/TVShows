db827d75f76a354a5dd7c70d4b9666e94b1b694e
Вячеслав Свиров <slavasvirov@MacBook-Pro-Vaceslav.local>
2021-03-10 15:11:31 +0300

    change height on full screen

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -5,15 +5,13 @@ import styles from "./Search.module.css";
 function Search({ onChange, filterByImage, placeholder, text }) {
   return (
     <div className={styles.inputBar}>
-      <Input autoFocus type='search' className={styles.inputRow} placeholder={placeholder} onChange={onChange} />
-      {/* <Input
+      <Input
         type="search"
         className={styles.inputRow}
         onChange={onChange}
         placeholder={placeholder}
-        size="800px"
         autoFocus
-      /> */}
+      />
       <Checkbox className={styles.checkboxCustom} onChange={filterByImage}>
         С изображениями
       </Checkbox>
